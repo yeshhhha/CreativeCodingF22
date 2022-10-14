8a0543f117474f75e67d385cf9ed0b4eec1781ed
yeshhhha <113397633+yeshhhha@users.noreply.github.com>
2022-10-14 11:31:52 -0400

    class

--- Week 5/script.js
+++ Week 5/script.js
@@ -1,31 +1,33 @@
-let toggle = false
+// let toggle = false
 
-function setup() {
-  createCanvas(windowWidth, windowHeight)
+// function setup() {
+//   createCanvas(windowWidth, windowHeight)
 
-  setTimeout(doThisLater, 4000)
-  setTimeout(doThisEvenLater, 6000)
-
-
-  setTimeout(() =>  {
-    console.log('Third Function = Loaded') 
-    toggle = true
-  }, 8000 )
-
-}
-
-function draw() {
-  background(100)
-  if(toggle) ellipse (100,100,100)
-  }
+//   setTimeout(doThisLater, 4000)
+//   setTimeout(doThisEvenLater, 6000)
 
 
-function doThisLater () {
-  console.log("Now the function has loaded")
-  toggle = true
-}
+//   setTimeout(() =>  {
+//     console.log('Third Function = Loaded') 
+//     toggle = true
+//   }, 8000 )
 
-const doThisEvenLater = () => {
-  console.log("this is the second function")
-  toggle = false
-}
+// }
+
+// function draw() {
+//   background(100)
+//   if(toggle) ellipse (100,100,100)
+//   }
+
+
+// function doThisLater () {
+//   console.log("Now the function has loaded")
+//   toggle = true
+// }
+
+// const doThisEvenLater = () => {
+//   console.log("this is the second function")
+//   toggle = false
+// }
+
+// const sum = (a,b) => a + b
