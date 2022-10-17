0e29a27967ceb339198ba434467ba6ee8fb3754f
yeshhhha <113397633+yeshhhha@users.noreply.github.com>
2022-10-17 17:08:49 -0400

    comments

--- API Integration/script.js
+++ API Integration/script.js
@@ -4,6 +4,7 @@ const client_id= "&client_id=V_F4G3tdOMiFNgHYXLWRgDetokuztdzelstvM6ifnXM";
 //   CC = loadImage('C.png')
 // }
 
+//Generating Random Number while avoiding decimals
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -12,25 +13,26 @@ function getRandomInt(min, max) {
 
 let random_number = getRandomInt(1, 20);
 console.log(random_number);
+//Using the random number generated to load a random page of Unsplash on the sketch with a certain query
 const api_url=`https://api.unsplash.com/search/collections?page=${random_number}&per_page=40&query=spooky`;
 
 let images = [];
+
 function setup() {
   createCanvas(windowWidth, 0)
 }
 
-
-
+// Creating the asynchronous function to fetch the API from the database
 async function photoSet(){
     const response = await fetch(api_url + client_id);
     const data = await response.json();
     console.log(data);
 
     
-    
+    //Plotting the results of the API onto the sketch
     images = data.results.map(s => s.preview_photos.map(a => a.urls["small"]));
 
-    
+    //Creating a container area for each image and displaying it
     //reference from http://jsfiddle.net/gcjkL5jw/3/ to display images
     let container = document.getElementById('imageContainer');
     let docFrag = document.createDocumentFragment();
@@ -48,5 +50,5 @@ async function photoSet(){
     
   }
 
-photoSet ()
+photoSet () //executing
 
