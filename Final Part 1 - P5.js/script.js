6768825b0c508d44911c7282efe15577e86a16f8
yeshhhha <113397633+yeshhhha@users.noreply.github.com>
2022-12-12 10:16:30 -0500

    Final

--- Final Part 1 - P5.js/script.js
+++ Final Part 1 - P5.js/script.js
@@ -3,7 +3,7 @@
 // After a mode has been selected the user can manipulate the elements with the ultrasonic sensors
 
 
-//Defining the global variables and arrays;
+//Defining the global variables and arrays
 let serial;
 let data;
 let value = [1.1, 0.55, ""];
@@ -92,8 +92,8 @@ let fireSketch = function (f) {
   //Fire Setup
   f.setup = function () {
     f.createCanvas(f.windowWidth, f.windowHeight, f.WEBGL); //adding WEBGL to enable 3D elements
-    f.angleMode(f.DEGREES) //setting the angle mode to degrees
-    f.noiseDetail(5) // defining how detailed the noise added should be
+    f.angleMode(f.DEGREES); //setting the angle mode to degrees
+    f.noiseDetail(5); // defining how detailed the noise added should be
   }
 
   //Fire Draw Loop
@@ -118,16 +118,16 @@ let fireSketch = function (f) {
 
     for (let i = 0; i < f.width; i += space) {
 
-      let xOff = f.map(f.cos(i), -1, 1, 0, 3) //defining how much the x angle should be offset everytime a torus is generated
-      let yOff = f.map(f.sin(i), -1, 1, 0, 3) //defining how much the y angle should be offset everytime a torus is generated
+      let xOff = f.map(f.cos(i), -1, 1, 0, 3); //defining how much the x angle should be offset everytime a torus is generated
+      let yOff = f.map(f.sin(i), -1, 1, 0, 3); //defining how much the y angle should be offset everytime a torus is generated
 
-      let n = f.noise(xOff, yOff + start) //mapping the noise element with the start position
+      let n = f.noise(xOff, yOff + start); //mapping the noise element with the start position
 
-      let h = f.map(n, 0, 1, -150, 150) //defining the height & mapping the position
+      let h = f.map(n, 0, 1, -150, 150); //defining the height & mapping the position
 
-      let r = f.map(h, 255, 255, 100, 55) //mapping the r value for random ranges in red
-      let g = f.map(h, 150, 255, 180, 245) //mapping the g value for random ranges in green
-      let b = f.map(h, 200, 255, 0, 155) // mapping the b value for random ranges in blue
+      let r = f.map(h, 255, 255, 100, 55); //mapping the r value for random ranges in red
+      let g = f.map(h, 150, 255, 180, 245); //mapping the g value for random ranges in green
+      let b = f.map(h, 200, 255, 0, 155); // mapping the b value for random ranges in blue
 
       f.fill(r, g, b); //fill colour of the elements
 
@@ -138,7 +138,7 @@ let fireSketch = function (f) {
 
     }
 
-    start += 0.05 //start position & speed
+    start += 0.05; //start position & speed
 
     //3D Fire Ring
     //Inspired by: ColourfulCoding https://www.youtube.com/watch?v=0YvPgYDR1oM&ab_channel=ColorfulCoding
@@ -146,33 +146,33 @@ let fireSketch = function (f) {
     f.pop();
 
     f.push();
-    f.scale(scaleValue / 2.015) //adjusting the scale to match the 3D torus 
-    f.translate(-f.width / 55, -f.height / 42) //adjusting the position to match the 3D torus
-    f.noStroke()
+    f.scale(scaleValue / 2.015); //adjusting the scale to match the 3D torus 
+    f.translate(-f.width / 55, -f.height / 42); //adjusting the position to match the 3D torus
+    f.noStroke();
 
-    let spaceF = 0.1 //defining the space and rate generated
+    let spaceF = 0.1; //defining the space and rate generated
 
     for (let i = 0; i < f.width; i += spaceF) {
 
-      let xfOff = f.map(f.cos(i), -10, 10, 10, 23) //defining the X offset angle
-      let yfOff = f.map(f.sin(i), -10, 1, 10, 23) //defining the Y offset angle
+      let xfOff = f.map(f.cos(i), -10, 10, 10, 23); //defining the X offset angle
+      let yfOff = f.map(f.sin(i), -10, 1, 10, 23); //defining the Y offset angle
 
-      let nf = f.noise(xfOff + start, yfOff + start) //mapping the noise element witht he start position
+      let nf = f.noise(xfOff + start, yfOff + start); //mapping the noise element witht he start position
 
-      let hf = f.map(nf, 0, 1, -150, 150) //defining the height & mapping the position
+      let hf = f.map(nf, 0, 1, -150, 150); //defining the height & mapping the position
 
-      let rf = f.map(f.sin(i), 255, 255, 100, 55) //mapping the r value for random ranges in red
-      let gf = f.map(hf, 150, 255, 180, 245) //mapping the g values for random ranges in green
-      let bf = f.map(nf, 200, 255, 0, 200) //mapping the b values for random ranges in blue
+      let rf = f.map(f.sin(i), 255, 255, 100, 55); //mapping the r value for random ranges in red
+      let gf = f.map(hf, 150, 255, 180, 245); //mapping the g values for random ranges in green
+      let bf = f.map(nf, 200, 255, 0, 200); //mapping the b values for random ranges in blue
 
       f.fill(rf, gf, bf); // fill colour of the elements
 
       f.rotate(spaceF); //rotating the ring with the amount of space
-      f.rect(400, 0, hf, 1) //generating multiple squares to look like blobs with the amount of noise
+      f.rect(400, 0, hf, 1); //generating multiple squares to look like blobs with the amount of noise
 
     }
 
-    start += 0.01 //start position & speed
+    start += 0.01; //start position & speed
     f.pop();
 
   }
@@ -189,20 +189,20 @@ let waterSketch = function (w) {
   //Water Setup
   w.setup = function () {
     w.createCanvas(w.windowWidth, w.windowHeight);
-    w.angleMode(w.DEGREES) //setting the angle mode to degrees
-    w.noiseDetail(10) // defining how detailed the noise added should be
+    w.angleMode(w.DEGREES); //setting the angle mode to degrees
+    w.noiseDetail(10); // defining how detailed the noise added should be
 
-    let density = 150 //defining the amount of waves generated
-    let space = w.width / density //defining the amount of space between each wave and the length
+    let density = 150; //defining the amount of waves generated
+    let space = w.width / density; //defining the amount of space between each wave and the length
 
     for (let x = 0; x < w.width; x += space) {
       for (let y = 0; y < w.height; y += space) {
-        let p = w.createVector(x + w.random(-10, 10), y + w.random(-10, 10)) //creating the waves
-        water.push(p) //pushing the waves to the canvas
+        let p = w.createVector(x + w.random(-10, 10), y + w.random(-10, 10)); //creating the waves
+        water.push(p); //pushing the waves to the canvas
       }
     }
 
-    multiplier = w.random(0.002, 0.01) //defining how many times and the length of each wave
+    multiplier = w.random(0.002, 0.01); //defining how many times and the length of each wave
 
   }
 
@@ -213,18 +213,18 @@ let waterSketch = function (w) {
 
     for (let i = 0; i < water.length; i++) {
 
-      let r = w.map(water[i].x, 0, w.width, 50, 65) //mapping the r value for random ranges in red
-      let g = w.map(water[i].y, 0, w.height, 60, 90) //mapping the g value for random ranges in green
-      let b = w.map(water[i].x, 0, w.width, 255, 150) //mapping the b value for random ranges in blue
+      let r = w.map(water[i].x, 0, w.width, 50, 65); //mapping the r value for random ranges in red
+      let g = w.map(water[i].y, 0, w.height, 60, 90); //mapping the g value for random ranges in green
+      let b = w.map(water[i].x, 0, w.width, 255, 150); //mapping the b value for random ranges in blue
 
       w.fill(r, g, b); //fill colour of the waves
 
-      let angle = w.map(w.noise(water[i].x * multiplier, water[i].y * multiplier), 0, 1, 0, (value[0] + value[1])) //defining the angle at which the waves should be generated using the ultrasonic sensors
+      let angle = w.map(w.noise(water[i].x * multiplier, water[i].y * multiplier), 0, 1, 0, (value[0] + value[1])); //defining the angle at which the waves should be generated using the ultrasonic sensors
 
-      water[i].add(w.createVector(w.cos(angle+value[0]), w.sin(angle+value[0]))) //Creating random waves based on the ultrasonic sensor values
+      water[i].add(w.createVector(w.cos(angle+value[0]), w.sin(angle+value[0]))); //Creating random waves based on the ultrasonic sensor values
 
       if (w.dist(w.width / 2, w.height / 2, water[i].x, water[i].y) < 300) { //creating a circular mask around the ellipse | //Inspired by: Colourful Coding https://www.youtube.com/watch?v=1-QXuR-XX_s&ab_channel=ColorfulCoding
-        w.ellipse(water[i].x, water[i].y, 1)
+        w.ellipse(water[i].x, water[i].y, 1);
       }
 
       if (!onScreen(water[i])) { //defining that if the waves go off the screen to generate a new one at a random x and y position
@@ -257,7 +257,7 @@ let earthSketch = function (e) {
   //Earth Setupt
   e.setup = function () {
     e.createCanvas(e.windowWidth, e.windowHeight, e.WEBGL);
-    e.angleMode(e.DEGREES) //setting the angle mode to degrees
+    e.angleMode(e.DEGREES); //setting the angle mode to degrees
 
     //mapping out the terrain to it's values
     for (let x = 0; x < 60; x++) {
@@ -280,15 +280,15 @@ let earthSketch = function (e) {
     e.stroke('#2BB02B');
     e.noFill();
     e.strokeWeight(2.5);
-    e.scale(scaleValue / 2.015) //Defining how large the terrain should be
+    e.scale(scaleValue / 2.015); //Defining how large the terrain should be
     zincline = (value[0] + 1) % 500; //Adjusting the incline position with the first ultrasonic sensor
     // incline = value[0]; //to move up and down
     e.translate(150, incline * 10, zincline); // adjusting the position of the terrain
     e.rotateX(85); // adding rotation to the background to the terrain looks like it is on flat ground
     e.translate(-e.width / 2, -e.height / 2);
-    for (let y = 0; y < 60; y++) {
+    for (let y = 0; y < 60; y++); {
       e.beginShape(e.TRIANGLE_STRIP); // generating the terrain using the p5 TRIANGLE_STRIP element | Inspired by: Hritik RC https://www.youtube.com/watch?v=_Tyhfpxwips&ab_channel=HritikRC
-      for (let x = 0; x < 60; x++) {
+      for (let x = 0; x < 60; x++); {
         //defining the x & y posisitons of each vector created
         e.vertex(x * 20, y * 20, terrain[x][y]);
         e.vertex(x * 20, (y + 1) * 20, terrain[x][y]);
